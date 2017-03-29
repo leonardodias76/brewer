0d934d542c7ee60b65c22c133944653f8f4d58dd
Leonardo Dias de Oliveira <leonardo.dias.oliveira@gmail.com>
2017-03-28 21:55:26 -0300

    Redimensionando a imagem - salvando o thumbnail

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -22,6 +22,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.brewer.validation.SKU;
 
@@ -86,6 +87,10 @@ public class Cerveja implements Serializable {
 	
 	public String getFoto() {
 		return foto;
+	}
+	
+	public String getFotoOuMock() {
+		return !StringUtils.isEmpty(foto) ? foto : "cerveja-mock.png";
 	}
 
 	public void setFoto(String foto) {
--- src/main/java/com/algaworks/brewer/service/CadastroCervejaService.java
+++ src/main/java/com/algaworks/brewer/service/CadastroCervejaService.java
@@ -1,20 +1,26 @@
 package com.algaworks.brewer.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.repository.CervejasRepository;
+import com.algaworks.brewer.service.event.cerveja.CervejaSalvaEvent;
 
 @Service
 public class CadastroCervejaService {
 
 	@Autowired
 	private CervejasRepository cervejasRepository;
-	
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@Transactional
 	public void save(Cerveja cerveja) {
 		cervejasRepository.save(cerveja);
+		publisher.publishEvent(new CervejaSalvaEvent(cerveja));
 	}
 }
