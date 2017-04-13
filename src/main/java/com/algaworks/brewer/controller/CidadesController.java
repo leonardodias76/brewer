a2231a947015997753d5913842f6ecd14340e1c9
Leonardo Dias de Oliveira <leonardo.dias.oliveira@gmail.com>
2017-04-12 21:29:05 -0300

    Selecionado cidades pelo estado com Ajax

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -1,13 +1,34 @@
 package com.algaworks.brewer.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.repository.CidadesRepository;
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadesController {
 
-	@RequestMapping("/cidades/nova")
+	@Autowired
+	private CidadesRepository cidadesRepository;
+	
+	@RequestMapping("/nova")
 	public String nova() {
 		return "cidade/CadastroCidade";
 	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(
+			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {
+
+		return cidadesRepository.findByEstadoCodigo(codigoEstado);
+	}
+	
 }
