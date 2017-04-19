4b5e302b0ab8a36211c88a8435833641cdafff62
Leonardo Dias de Oliveira <leonardo.dias.oliveira@gmail.com>
2017-04-19 20:55:28 -0300

    Pesquisa de cidades

--- src/main/java/com/algaworks/brewer/repository/CidadesRepository.java
+++ src/main/java/com/algaworks/brewer/repository/CidadesRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.brewer.model.Cidade;
 import com.algaworks.brewer.model.Estado;
+import com.algaworks.brewer.repository.helper.cidade.CidadesRepositoryQueries;
 
-public interface CidadesRepository extends JpaRepository<Cidade, Long> {
+public interface CidadesRepository extends JpaRepository<Cidade, Long>, CidadesRepositoryQueries {
 
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
 	
