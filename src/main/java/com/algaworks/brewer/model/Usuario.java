eeef11dfb7275539a8300805289ce101e366b955
Leonardo Dias de Oliveira <leonardo.dias.oliveira@gmail.com>
2017-05-04 21:33:52 -0300

    Organização dos imports

--- src/main/java/com/algaworks/brewer/model/Usuario.java
+++ src/main/java/com/algaworks/brewer/model/Usuario.java
@@ -14,7 +14,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
