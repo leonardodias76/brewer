31a66fa0100f72c59de8357793db8ed400df0bb3
Leonardo Dias de Oliveira <leonardo.dias.oliveira@gmail.com>
2017-07-24 23:23:37 -0300

    Consultas em arquivos externos

--- src/main/java/com/algaworks/brewer/repository/helper/venda/VendasRepositoryImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/venda/VendasRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.algaworks.brewer.repository.helper.venda;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.MonthDay;
 import java.time.Year;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
@@ -23,6 +25,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.algaworks.brewer.dto.VendaMes;
 import com.algaworks.brewer.model.StatusVenda;
 import com.algaworks.brewer.model.TipoPessoa;
 import com.algaworks.brewer.model.Venda;
@@ -86,6 +89,26 @@ public class VendasRepositoryImpl implements VendasRepositoryQueries {
 					.setParameter("status", StatusVenda.EMITIDA)
 					.getSingleResult());
 		return optional.orElse(BigDecimal.ZERO);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<VendaMes> totalPorMes() {
+		List<VendaMes> vendasMes = manager.createNamedQuery("Vendas.totalPorMes").getResultList();
+		
+		LocalDate hoje = LocalDate.now();
+		for (int i = 1; i <= 6; i++) {
+			String mesIdeal = String.format("%d/%02d", hoje.getYear(), hoje.getMonthValue());
+			
+			boolean possuiMes = vendasMes.stream().filter(v -> v.getMes().equals(mesIdeal)).findAny().isPresent();
+			if (!possuiMes) {
+				vendasMes.add(i - 1, new VendaMes(mesIdeal, 0));
+			}
+			
+			hoje = hoje.minusMonths(1);
+		}
+		
+		return vendasMes;
 	}
 	
 	private Long total(VendaFilter filtro) {
