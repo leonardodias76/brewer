b4107ec75cadb48d9503b430e7a13b7ba75b9138
Leonardo Dias de Oliveira <leonardo.dias.oliveira@gmail.com>
2017-07-24 22:27:01 -0300

    Box de vendas no ano, no mês e ticket médio

--- src/main/java/com/algaworks/brewer/controller/DashboardController.java
+++ src/main/java/com/algaworks/brewer/controller/DashboardController.java
@@ -1,16 +1,24 @@
 package com.algaworks.brewer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.brewer.repository.VendasRepository;
+
 @Controller
 public class DashboardController {
+	
+	@Autowired
+	private VendasRepository vendasRepository;
 
 	@GetMapping("/")
 	public ModelAndView dashboard() {
 		ModelAndView mv = new ModelAndView("Dashboard");
-
+		mv.addObject("vendasNoAno", vendasRepository.valorTotalNoAno());
+		mv.addObject("vendasNoMes", vendasRepository.valorTotalNoMes());
+		mv.addObject("ticketMedio", vendasRepository.valorTicketMedioNoAno());
 		return mv;
 	}
 }
